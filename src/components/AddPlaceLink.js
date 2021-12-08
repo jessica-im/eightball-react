93fb0368a2ae0f2e21db458f0484d3a1424463ce
Jessica Im <me@jessicaim.com>
2021-12-08 16:00:16 -0500

    saving in desktop styling

--- src/components/AddPlaceLink.js
+++ src/components/AddPlaceLink.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const AddPlaceLink = () => {
 
      return (
+
           <div className="add-link-container">
                <Link to="/new" className="add-link footer">+</Link>
           </div>
